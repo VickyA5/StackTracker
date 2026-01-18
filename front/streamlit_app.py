c018292a045101fd5ec42d97258a830e7cd0a4e6
VickyA5 <vavalos@fi.uba.ar>
2026-01-18 15:09:16 +0000

    chore: cleaning repo

--- front/streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-# Forwarder page to the new main file name
-st.set_page_config(page_title="StackTracker - Login", page_icon="📦")
-st.info("Forwarding to Login…")
-st.switch_page("Login.py")
